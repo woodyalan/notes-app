f811e64fb7d040ed27326c60b4068ebce0bb2645
Alan Santos <alan.afs89@gmail.com>
2021-04-20 18:57:40 -0300

    Inicio do projeto

---
+++ pages/index.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div v-for="nota of notas" :key="nota.id" class="col-md-3">
+        <div class="card bg-warning my-3">
+          <div class="card-body">
+            <h5 class="card-title">{{ nota.titulo }}</h5>
+            <p v-if="nota.descricao" class="card-text">
+              {{ nota.descricao }}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      notas: [
+        {
+          id: 1,
+          titulo: "Título da nota",
+          descricao: "Descrição da nota"
+        },
+        {
+          id: 2,
+          titulo: "Título da nota 123",
+          descricao: null
+        }
+      ]
+    };
+  }
+};
+</script>
+
+<style>
+.card {
+  min-height: 15rem;
+}
+</style>
