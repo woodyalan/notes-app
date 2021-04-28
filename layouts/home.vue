7d64eedaf143a966bec51905ded9d0ca419a4fef
Alan Santos <alan.afs89@gmail.com>
2021-04-27 21:55:42 -0300

    Layout home criado e configurado

---
+++ layouts/home.vue
@@ -0,0 +1,48 @@
+<template>
+  <div>
+    <b-navbar type="dark" variant="dark">
+      <b-navbar-brand href="/">Notes App</b-navbar-brand>
+      <b-navbar-toggle target="notesBar"></b-navbar-toggle>
+
+      <b-collapse id="notesBar" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item-dropdown :text="primeiroNome" right>
+            <b-dropdown-item href="perfil">Perfil</b-dropdown-item>
+            <b-dropdown-item href="#" @click.prevent="logout"
+              >Sair</b-dropdown-item
+            >
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+
+    <div class="container">
+      <Nuxt />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    usuario() {
+      return this.$store.state.auth.user;
+    },
+    primeiroNome() {
+      if (this.usuario) {
+        const [nome] = this.usuario.nome.split(" ");
+
+        return nome;
+      }
+    }
+  },
+  methods: {
+    async logout() {
+      await this.$auth.logout();
+      this.$router.push("/login");
+    }
+  }
+};
+</script>
+
+<style></style>
--- pages/perfil/index.vue
+++ pages/perfil/index.vue
@@ -1,7 +1,9 @@
 <template> </template>
 
 <script>
-export default {};
+export default {
+  layout: "home"
+};
 </script>
 
 <style></style>
